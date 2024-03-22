8379e24ab256331ddbe69446201a889854821c94
dansad1 <dansadkov@yandex.ru>
2024-03-22 21:36:12 +0300

    поменял бд на mysql

--- servicedesk/settings.py
+++ servicedesk/settings.py
@@ -79,12 +79,18 @@ WSGI_APPLICATION = 'servicedesk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'service',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',  # Или IP-адрес вашего сервера MySQL
+        'PORT': '3306',       # Порт по умолчанию для MySQL
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
