ed21c6808989cb5203de7e3214e49378f2333cb9
dansad1 <dansadkov@yandex.ru>
2024-04-14 19:47:08 +0300

    упростил активы (все еще не готово

--- service/views/asset_type_views.py
+++ service/views/asset_type_views.py
@@ -1,45 +1,36 @@
+from django.forms import modelformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 
-from service.models import AssetType
-from service.forms.Asset_Forms import AssetTypeForm  # Предполагается, что у вас есть форма для модели AssetType
+from service.models import AssetType, Attribute
+from service.forms.Asset_Forms import AssetTypeForm
 
-# Создание типа актива
-def create_asset_type(request):
+def asset_type_create(request):
     if request.method == 'POST':
         form = AssetTypeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('asset_type_list')  # URL к списку типов активов
+            return redirect('asset_type_list')
     else:
         form = AssetTypeForm()
     return render(request, 'asset_types/asset_type_create.html', {'form': form})
-
-
-# Редактирование типа актива
-def edit_asset_type(request, pk):
-    asset_type = AssetType.objects.get(pk=pk)
+def asset_type_list(request):
+    asset_types = AssetType.objects.all()
+    return render(request, 'asset_types/asset_type_list.html', {'asset_types': asset_types})
+def asset_type_edit(request, pk):
+    asset_type = get_object_or_404(AssetType, pk=pk)
     if request.method == 'POST':
         form = AssetTypeForm(request.POST, instance=asset_type)
         if form.is_valid():
             form.save()
-            return redirect('asset_type_list')  # Обновлено для перенаправления на список типов активов
+            return redirect('asset_type_list')
     else:
         form = AssetTypeForm(instance=asset_type)
-    return render(request, 'asset_types/asset_type_edit.html', {'form': form})
-
-
-# Вывод списка типов актива
-def asset_type_list(request):
-    asset_types = AssetType.objects.all()
-    return render(request, 'asset_types/asset_type_list.html', {'asset_types': asset_types})
-
-
-# Удаление типа актива
+    # Передаем asset_type в контекст
+    return render(request, 'asset_types/asset_type_edit.html', {'form': form, 'asset_type': asset_type})
 def asset_type_delete(request, pk):
-    if request.method == 'POST':
-        asset_type = get_object_or_404(AssetType, pk=pk)
-        asset_type.delete()
+    asset_type = AssetType.objects.get(pk=pk)
+    asset_type.delete()
     return redirect('asset_type_list')
