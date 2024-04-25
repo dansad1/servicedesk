c483fab70be14b000ea256b94b72e930ac3ba581
dansad1 <dansadkov@yandex.ru>
2024-04-25 13:58:43 +0300

    активы создаются атрибуты пока не отображаются

--- service/views/asset_views.py
+++ service/views/asset_views.py
@@ -1,33 +1,33 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from service.forms.Asset_Forms import AssetForm
-from service.models import AssetAttribute, Attribute, Asset
+from service.models import AssetAttribute, Attribute, Asset, AssetTypeAttribute
+
 
 # Создание актива
 def create_asset(request):
     if request.method == 'POST':
         form = AssetForm(request.POST)
         if form.is_valid():
-            # Saves the asset and returns the new object
+            # Сохраняем актив и возвращаем созданный объект
             asset = form.save()
 
-            # Iterate over dynamically added attribute fields
+            # Итерация по динамически добавленным полям атрибутов
             for attribute in Attribute.objects.all():
                 field_name = f'attribute_{attribute.pk}_value'
                 if field_name in request.POST:
                     attribute_value = request.POST.get(field_name, '')
-                    # Create and link AssetAttribute only if a value is provided
+                    # Создаем и связываем AssetAttribute, только если предоставлено значение
                     if attribute_value:
                         AssetAttribute.objects.create(
                             asset=asset,
                             attribute=attribute,
-                            value_text=attribute_value  # Assuming text value, adjust based on attribute type
+                            value_text=attribute_value  # Предполагается текстовое значение, настройте в зависимости от типа атрибута
                         )
 
-            # Redirect to the asset list page after the asset and its attributes are saved
+            # Перенаправление на страницу списка активов после сохранения актива и его атрибутов
             return redirect('assets_list')
-
     else:
         form = AssetForm()
 
@@ -79,3 +79,6 @@ def delete_asset(request, pk):
 def asset_list(request):
     assets = Asset.objects.prefetch_related('components', 'asset_attributes').all()
     return render(request, 'assets/asset_list.html', {'assets': assets})
+def get_attributes_by_asset_type(request, asset_type_id):
+    attributes = list(AssetTypeAttribute.objects.filter(asset_type_id=asset_type_id).values('id', 'attribute__name', 'attribute__attribute_type'))
+    return JsonResponse(attributes, safe=False)
