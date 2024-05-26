a3aa1d3ec48f9228f910fe3fe80431549a2673dc
Egor Sidorov <sidorov.egor.main@gmail.com>
2024-05-26 15:02:20 +0300

    edit view for performer groups

--- service/forms/Performer_forms.py
+++ service/forms/Performer_forms.py
@@ -5,3 +5,7 @@ class PerformerGroupForm(forms.ModelForm):
     class Meta:
         model = PerformerGroup
         fields = ['name', 'description', ]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название группы'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите описание группы'}),
+        }
